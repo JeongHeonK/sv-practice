1df5fda6f9570c5398b0cf557c9d51c8edbf6aa8
JeongHeon Kim <kdrlawjdgjs@gmail.com>
2026-03-29 19:41:04 +0900

    feat(component): DisplayName 컴포넌트 추가

---
+++ src/lib/Components/DisplayName.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	let firstName = $state('jh');
+	let lastName = $state('kim');
+	let fullNmae = $derived(firstName.concat(lastName));
+	const src = 'https://picsum.photos/250/250';
+
+	const doubledName = () => {
+		firstName += firstName;
+	};
+</script>
+
+<h1>Welcome {fullNmae}</h1>
+<button type="button" onclick={doubledName}>click</button>
+<img {src} alt="something" />
+<input bind:value={firstName} />
+
+<style>
+	h1 {
+		font-size: 48px;
+	}
+
+	button {
+		padding: 4px 10px;
+		border: 1px solid black;
+		border-radius: 12px;
+	}
+</style>
